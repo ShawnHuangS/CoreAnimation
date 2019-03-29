43c9798ecb9b87c5fd63372fd2921f33395bdcb8
ShawnHuang <iesel.apple@gmail.com>
2019-03-29 12:16:26 +0800

    use anchorPoint and transform to make clock

--- CoreAnimation/LayerGeometryViewController.swift
+++ CoreAnimation/LayerGeometryViewController.swift
@@ -10,28 +10,64 @@ import UIKit
 
 class LayerGeometryViewController: UIViewController {
     
-    @IBOutlet weak var hourView: UIView!
+    @IBOutlet weak var hourView: UIView!{
+        didSet{
+            hourView.layer.anchorPoint = CGPoint(x: 0.5, y: 1)
+        }
+    }
     
-    @IBOutlet weak var minuteView: UIView!
+    @IBOutlet weak var minuteView: UIView! {
+        didSet{
+            minuteView.layer.anchorPoint = CGPoint(x: 0.5, y: 1)
+        }
+    }
     
-    @IBOutlet weak var secondView: UIView!
+    @IBOutlet weak var secondView: UIView! {
+        didSet{
+            secondView.layer.anchorPoint = CGPoint(x: 0.5, y: 1)
+        }
+    }
     
     let layerView = UIView()
     override func viewDidLoad() {
         super.viewDidLoad()
-       
-        hourView.layer.anchorPoint = CGPoint(x: 0.5, y: 1)
-        minuteView.layer.anchorPoint = CGPoint(x: 0.5, y: 1)
-        secondView.layer.anchorPoint = CGPoint(x: 0.5, y: 1)
-        // Do any additional setup after loading the view.
         
+        
+//        CADisplayLink精確度高
+//        NSTimer佔用系統資源較多
+//        CADisplayLink本來就在進程中，每秒進行60次。
+//        核心動畫的時候，最好使用CADisplayLink
+
+        let link = CADisplayLink.init(target: self, selector: #selector(LayerGeometryViewController.clockRun))
+        link.add(to: .main, forMode: .default)
         
     }
     override func viewDidAppear(_ animated: Bool) {
+        
 
-      
-   
-
+    }
+    
+    
+    @objc func clockRun(){
+        let tZone = TimeZone.current
+        var calendar = Calendar.current
+        let currentDate = Date()
+        
+        calendar.timeZone = tZone
+        
+        let currentTime = calendar.dateComponents([.hour,.minute,.second], from: currentDate)
+ 
+        let secondAngle = CGFloat(Double(currentTime.second!) * (Double.pi * 2 / 60))
+        secondView.transform = CGAffineTransform.init(rotationAngle: CGFloat(secondAngle))
+        
+        let minuteAngle = CGFloat ( Double(currentTime.minute!) * (Double.pi * 2.0 / 60) )
+        minuteView.transform = CGAffineTransform(rotationAngle: minuteAngle)
+        
+//        let hourAngle = CGFloat ( Double(currentTime.hour!) * (Double.pi * 2.0 / 12) )
+        let hourAngle = CGFloat ((Double(currentTime.hour!) + Double(currentTime.minute!) / 60.0) * (Double.pi * 2.0 / 12) )
+        
+        hourView.transform = CGAffineTransform(rotationAngle: hourAngle)
+        
     }
 
     /*
